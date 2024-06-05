286cb7ce8732d4485a7cc115f420ea7b14ddbaa5
coolbeangeeks <abhigupta96677@gmail.com>
2024-06-06 00:52:26 +0530

    added a functionality of hover andhover out

--- StarRatingComponent/app.js
+++ StarRatingComponent/app.js
@@ -1,19 +1,35 @@
 const stars = document.querySelectorAll('.bi-star-fill');
 const ratingValue = document.getElementById('rating-value');
-
+let currentRating = 0;
 
 stars.forEach(star => {
     star.addEventListener('click', () => {
         const index = parseInt(star.getAttribute('data-index'));
-
-        stars.forEach((s, i) => {
-            if (i < index) {
-                s.classList.add('filled');
-            } else {
-                s.classList.remove('filled');
-            }
-        });
-
-        ratingValue.textContent = index;
+        currentRating=index
+        updateStars(currentRating);
+        ratingValue.textContent = currentRating;
     });
+    star.addEventListener('mouseover', () => {
+        const index = parseInt(star.getAttribute('data-index'));
+        updateStars(index);
+        ratingValue.textContent = index;
+
+
+    });
+    star.addEventListener('mouseout', () => {
+        updateStars(currentRating);
+        ratingValue.textContent = currentRating;
+
+    });
+    
 });
+function updateStars(index) {
+    console.log(`update ${index} value`);
+    stars.forEach((s, i) => {
+        if (i < index) {
+            s.classList.add('filled');
+        } else {
+            s.classList.remove('filled');
+        }
+    });
+}
